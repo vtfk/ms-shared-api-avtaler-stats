a2a72b029b1b830578cc4662e5c82baebe200ad6
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-08-15 21:16:23 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ms-shared-api-avtaler-stats
+
 Stats service for avtaler
+
+## Deploy
+
+This service is created to run on the [ZEIT/Now](https://zeit.co/now) serverless infrastructure.
+
+Make sure the settings in [now.json](now.json) matches your environment.
+
+Run the deploy script.
+
+```
+$ npm run deploy
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ api/total.js
@@ -0,0 +1,27 @@
+const mongo = require('../lib/mongo')
+const logger = require('../lib/logger')
+
+function getParams (url) {
+  const list = url.split('/')
+  return {
+    type: list[3]
+  }
+}
+
+module.exports = async (request, response) => {
+  const db = await mongo()
+  const logs = db.collection(process.env.MONGODB_COLLECTION)
+  const params = getParams(request.url)
+  const { type } = params
+  const query = type ? { type: type } : {}
+  logger('info', ['api', 'total', 'type', type || 'any'])
+  try {
+    const count = await logs.countDocuments(query)
+    logger('info', ['api', 'total', 'success', count])
+    response.json({ total: count })
+  } catch (error) {
+    logger('error', ['api', 'total', error])
+    response.status(500)
+    response.send(error)
+  }
+}
